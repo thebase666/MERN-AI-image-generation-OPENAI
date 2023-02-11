a632e758a918b950f7e5b15abd9e6890fb3ceaef
yours <lizhuyao666@gmail.com>
2023-02-11 10:49:17 +0800

    update server address

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -30,7 +30,7 @@ const CreatePost = () => {
         setGeneratingImg(true);
         console.log('1', form);
 
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://mern-ai-image-generation-openai.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -61,7 +61,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://mern-ai-image-generation-openai.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
